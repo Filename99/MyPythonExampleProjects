2fd6a6000264289e0de415fe4fd76c24ca1f036a
Filename99 <71535927+Filename99@users.noreply.github.com>
2023-12-19 12:05:00 +0900

    Update Simple Calculator.py

--- Simple Calculator.py
+++ Simple Calculator.py
@@ -1,48 +1,31 @@
-# A simple calculator application in Python
-
-# Function to add two numbers
-def add(num1, num2):
-    return num1 + num2
-
-# Function to subtract two numbers
-def subtract(num1, num2):
-    return num1 - num2
-
-# Function to multiply two numbers
-def multiply(num1, num2):
-    return num1 * num2
-
-# Function to divide two numbers
-def divide(num1, num2):
-    return num1 / num2
-
-# Main program
-while True:
-    print("Select operation.")
-    print("1. Add")
-    print("2. Subtract")
-    print("3. Multiply")
-    print("4. Divide")
-
-    # Take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
-
-    # Check if choice is one of the four options
-    if choice in ('1', '2', '3', '4'):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
-
-        if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
-
-        elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
-
-        elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
-
-        elif choice == '4':
-            print(num1, "/", num2, "=", divide(num1, num2))
-        break
+def calculator():
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    
+    print("Select operation:")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    
+    choice = int(input("Enter your choice (1-4): "))
+    
+    if choice == 1:
+        result = num1 + num2
+        print("The result is:", result)
+    elif choice == 2:
+        result = num1 - num2
+        print("The result is:", result)
+    elif choice == 3:
+        result = num1 * num2
+        print("The result is:", result)
+    elif choice == 4:
+        if num2 == 0:
+            print("Error: Division by zero is not allowed.")
+        else:
+            result = num1 / num2
+            print("The result is:", result)
     else:
-        print("Invalid Input")
+        print("Invalid choice. Please try again.")
+
+calculator()
