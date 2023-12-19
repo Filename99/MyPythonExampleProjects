324c38935c5e3b0a7edc912ac01774b275a039ff
Filename99 <71535927+Filename99@users.noreply.github.com>
2023-12-19 12:13:22 +0900

    Update Day of the Week based on Date and Year.py

--- Day of the Week based on Date and Year.py
+++ Day of the Week based on Date and Year.py
@@ -1,16 +1,12 @@
 import datetime
 
-# get user input for date
-user_date = input("Enter a date (MM/DD): ")
+date_input = input("Enter a date in the format MM/DD: ")
 
-# get current year
 current_year = datetime.datetime.now().year
 
-# combine user input with current year to create datetime object
-user_date = datetime.datetime.strptime(f"{user_date}/{current_year}", "%m/%d/%Y")
+date_string = f"{date_input}/{current_year}"
+date_object = datetime.datetime.strptime(date_string, "%m/%d/%Y")
 
-# get day of the week
-day_of_week = user_date.strftime("%A")
+day_of_week = date_object.strftime("%A")
 
-# display result
-print(f"The day of the week for {user_date.strftime('%m/%d/%Y')} is {day_of_week}.")
+print(f"The day of the week for {date_input}/{current_year} is {day_of_week}.")
